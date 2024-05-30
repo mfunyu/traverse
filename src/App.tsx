21663e0038bb5603b133525663f82ea1c1be24a1
mfunyu <mfunyu@student.42lyon.fr>
2024-05-30 14:17:27 +0000

    fix: add .ts to linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./styles/App.css";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          aaaaaaa.
         </p>
         <a
           className="App-link"
