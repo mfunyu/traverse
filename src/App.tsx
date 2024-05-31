d579d82930563b410dbcdc3d717e0d6e67687d7d
mfunyu <mfunyu@student.42lyon.fr>
2024-05-31 15:43:47 +0000

    add: Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./styles/App.css";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          aaaaaaa.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
     </div>
   );
 }
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/NavigationBar.css";
+
+const NavigationBar = () => {
+  return (
+    <div className="navbar">
+      <div className="brand">Traverse</div>
+      <div className="nav-buttons">
+        <button>Clear</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
