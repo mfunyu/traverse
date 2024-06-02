f30a61d34560ae7cf6da1cba3a577ccd5a9a314c
mfunyu <mfunyu@student.42tokyo.jp>
2024-06-02 15:01:04 +0000

    add: planner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles/App.css";
 import NavigationBar from "./components/NavigationBar";
+import Planner from "./components/Planner";
 import "./styles/Color.css";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Planner />
     </div>
   );
 }
---
+++ src/components/Planner.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/Planner.css";
+
+
+const Planner = () => {
+  return (
+    <div className="planner">
+      <div className="contents">
+        <div className="header">Trip to Paris</div>
+        <div className="lists">
+          <div className="box">Box 1</div>
+          <div className="box">Box 2</div>
+          <div className="box">Box 3</div>
+          <div className="box">Box 4</div>
+          <div className="box">Box 5</div>
+          <div className="box">Box 6</div>
+          <div className="box">Box 7</div>
+          <div className="box">Box 8</div>
+          <div className="box">Box 9</div>
+          <div className="box">Box 10</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Planner;
