d43ef6135b1c79b1e04143df2453312373cb1921
mfunyu <mfunyu@student.42tokyo.jp>
2024-06-02 14:53:49 +0000

    update: navigation bar, add color.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./styles/App.css";
 import NavigationBar from "./components/NavigationBar";
+import "./styles/Color.css";
 
 function App() {
   return (
