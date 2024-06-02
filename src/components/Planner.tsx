9f72f8dfcdf7ac8c507ac3b274d985680e6c4c87
mfunyu <mfunyu@student.42tokyo.jp>
2024-06-02 15:08:51 +0000

    update: separate planner functions

--- src/components/Planner.tsx
+++ src/components/Planner.tsx
@@ -1,24 +1,47 @@
 import React from "react";
 import "../styles/Planner.css";
 
+const PlannerItem = () => {
+  return (
+    <div className="item">
+      <div className="item-header">
+        <h3>Day 1</h3>
+      </div>
+      <div className="item-body">
+        <p>Visit the Eiffel Tower</p>
+        <p>Visit the Louvre Museum</p>
+        <p>Visit the Notre Dame Cathedral</p>
+      </div>
+    </div>
+  );
+};
+
+const PlannerLists = () => {
+  return (
+    <div className="lists">
+      <PlannerItem />
+      <PlannerItem />
+      <PlannerItem />
+      <PlannerItem />
+      <PlannerItem />
+    </div>
+  );
+};
+
+const PlannerHeader = () => {
+  return (
+    <div className="header">
+      <h2>Trip to Paris</h2>
+    </div>
+  );
+};
 
 const Planner = () => {
   return (
     <div className="planner">
       <div className="contents">
-        <div className="header">Trip to Paris</div>
-        <div className="lists">
-          <div className="box">Box 1</div>
-          <div className="box">Box 2</div>
-          <div className="box">Box 3</div>
-          <div className="box">Box 4</div>
-          <div className="box">Box 5</div>
-          <div className="box">Box 6</div>
-          <div className="box">Box 7</div>
-          <div className="box">Box 8</div>
-          <div className="box">Box 9</div>
-          <div className="box">Box 10</div>
-        </div>
+        <PlannerHeader/ >
+        <PlannerLists />
       </div>
     </div>
   );
