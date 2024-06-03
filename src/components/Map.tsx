ec62da5b6e8b0d96243766dcdcc67e7c889cdd32
mfunyu <mfunyu@student.42lyon.fr>
2024-06-03 10:48:49 +0000

    fix: css make map behind

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,7 +8,6 @@ function Map() {
       <MapContainer
         center={[51.505, -0.09]}
         zoom={13}
-        scrollWheelZoom={false}
       >
         <TileLayer
           attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
