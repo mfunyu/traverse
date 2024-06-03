5f1a4020807393da7aca1604d2232c5d66b13859
mfunyu <mfunyu@student.42lyon.fr>
2024-06-03 10:35:38 +0000

    add: render map

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavigationBar from "./components/NavigationBar";
 import Planner from "./components/Planner";
 import "./styles/App.scss";
+import Map from "./components/Map";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
       <Planner />
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.tsx
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "../styles/components/Map.scss";
+import "leaflet/dist/leaflet.css";
+
+function Map() {
+  return (
+    <div className="map">
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
