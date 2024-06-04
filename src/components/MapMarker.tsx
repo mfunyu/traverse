236761bcea33d1bf7b10d5dc19bfae70b856b5eb
mfunyu <mfunyu@student.42lyon.fr>
2024-06-04 11:01:03 +0000

    update: add markerInRoute / notInRoute

--- src/components/MapMarker.tsx
+++ src/components/MapMarker.tsx
@@ -2,22 +2,43 @@ import { Marker, Popup } from "react-leaflet";
 import { LatLngTuple } from "leaflet";
 import "../styles/components/MapMarker.scss";
 
+function MarkerInRoute() {
+
+  return (
+    <div className="popup-item">
+      <div className="text">
+        <div className="circle-number">1</div>
+        <div className="details">
+          <h3>Lyon</h3>
+          <p>69100 France </p>
+        </div>
+      </div>
+      <button>x Remove from route</button>
+    </div>
+  );
+}
+
+function MarkerNotInRoute() {
+  return (
+    <div className="popup-item">
+      <div className="text">
+        < div className="details">
+          <h3>Lyon</h3>
+          <p>69100 France </p>
+        </div >
+      </div >
+      <button>+ Add to route</button>
+    </div >
+  );
+}
+
 function MapMarker() {
   const position: LatLngTuple = [51.505, -0.09];
 
   return (
     <Marker position={position}>
       <Popup>
-        <div className="popup-item">
-          <div className="text">
-            <div className="circle-number">1</div>
-            <div className="details">
-              <h3>Lyon</h3>
-              <p>69100 France </p>
-            </div>
-          </div>
-          <button>Add to route</button>
-        </div>
+        <MarkerInRoute />
       </Popup>
     </Marker>
   );
