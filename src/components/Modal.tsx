6f52631572c217d7e34dc2c77adc06d5052c4428
mfunyu <mfunyu@student.42lyon.fr>
2024-06-05 10:06:15 +0000

    update: Modal css

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,23 +16,42 @@ function Modal() {
         <span className="close-button"/*  onClick={onClose} */>
           &times;
         </span>
-        <h2>Modifier la destination</h2>
+        <h2>Lyon</h2>
+        <p>69100, 60 Rue Marceille</p>
         <input
           /* value={name}
           onChange={(e) => setName(e.target.value)} */
+          placeholder="Lyon"
           className="modal-input"
+        />
+        <input
+          /* value={name}
+          onChange={(e) => setName(e.target.value)} */
+          placeholder="Not set"
+          className="modal-input"
+          type="date"
+        />
+        <input
+          /* value={name}
+          onChange={(e) => setName(e.target.value)} */
+          placeholder="0"
+          className="modal-input"
+          type="number"
         />
         <input
           /* value={address}
           onChange={(e) => setAddress(e.target.value)} */
+          placeholder="Add notes"
           className="modal-input"
         />
-        <button /* onClick={handleSave} */ className="modal-button">
-          Enregistrer
-        </button>
-        <button /* onClick={onClose} */ className="modal-button">
-          Annuler
-        </button>
+        <div className="modal-footer">
+          <button /* onClick={onClose} */ className="modal-button">
+            x remove destination
+          </button>
+          <button /* onClick={handleSave} */ className="modal-button">
+            save
+          </button>
+        </div>
       </div>
     </div>
   );
