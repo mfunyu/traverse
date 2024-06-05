5ca481cc41f48e7567d41297ef1b619a126cf516
mfunyu <mfunyu@student.42lyon.fr>
2024-06-05 11:50:03 +0000

    add: InputFiled

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,27 @@
 import React, { useState } from "react";
 import "../styles/components/Modal.scss";
 
+type Props = {
+  label: string;
+  placeholder: string;
+  type: string;
+}
+
+function InputField({ label, placeholder, type }: Props) {
+  return (
+    <div className="input-field">
+      <h4 className="label">{label}</h4>
+      <input
+        /* value={name}
+        onChange={(e) => setName(e.target.value)} */
+        placeholder={placeholder}
+        type={type}
+        min="0"
+      />
+    </div>
+  );
+}
+
 function Modal() {
 /*   const [name, setName] = useState(destination.name);
   const [address, setAddress] = useState(destination.address);
@@ -18,32 +39,10 @@ function Modal() {
         </span>
         <h2>Lyon</h2>
         <p>69100, 60 Rue Marceille</p>
-        <input
-          /* value={name}
-          onChange={(e) => setName(e.target.value)} */
-          placeholder="Lyon"
-          className="modal-input"
-        />
-        <input
-          /* value={name}
-          onChange={(e) => setName(e.target.value)} */
-          placeholder="Not set"
-          className="modal-input"
-          type="date"
-        />
-        <input
-          /* value={name}
-          onChange={(e) => setName(e.target.value)} */
-          placeholder="0"
-          className="modal-input"
-          type="number"
-        />
-        <input
-          /* value={address}
-          onChange={(e) => setAddress(e.target.value)} */
-          placeholder="Add notes"
-          className="modal-input"
-        />
+        <InputField label="Display name" placeholder="Lyon" type="text"/>
+        <InputField label="Arrival date" placeholder="undefined" type="date"/>
+        <InputField label="Length of stay" placeholder="0" type="number"/>
+        <InputField label="Notes" placeholder="add notes" type="text"/>
         <div className="modal-footer">
           <button /* onClick={onClose} */ className="modal-button">
             x remove destination
