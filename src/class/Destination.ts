9c5c6725d698c19853c123036c0fe6d34e7b397b
mfunyu <mfunyu@student.42lyon.fr>
2024-06-10 12:12:52 +0000

    add: id to destination

--- src/class/Destination.ts
+++ src/class/Destination.ts
@@ -1,7 +1,9 @@
 import { LatLngTuple } from "leaflet";
 import { DestinationObject } from "../types/destination";
+import { v4 as uuidv4 } from "uuid";
 
 class Destination implements DestinationObject {
+  readonly id: string;
   readonly label: string;
   readonly latLng: LatLngTuple;
   readonly address: string;
@@ -12,6 +14,7 @@ class Destination implements DestinationObject {
   notes: string | null;
 
   constructor(label: string, latLng: number[] | LatLngTuple, customLabel: string | null, arrivalDate: Date, lengthOfStay: number, notes: string | null) {
+    this.id = uuidv4();
     this.label = label.split(",")[0];
     this.latLng = [latLng[0], latLng[1]];
     this.address = label;
